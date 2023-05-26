d0ab8812757aaf4061964ca54c55903a62b2ec6d
Lucas Naidu <lnaidu@c2r1p4.42nice.fr>
2023-05-26 11:45:59 +0200

    base

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: lnaidu <lnaidu@student.42nice.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/17 11:56:41 by lnaidu            #+#    #+#              #
-#    Updated: 2023/05/25 13:18:04 by lnaidu           ###   ########.fr        #
+#    Updated: 2023/05/26 11:41:19 by lnaidu           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = Cub3d
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror -g
-SRC = 
+SRC = main.c
 #COLORS
 RED = \033[1;31m
 GREEN = \033[1;32m
@@ -22,6 +22,9 @@ DEFAULT = \033[0m
 
 
 OBJS = ${SRC:.c=.o}
+
+.c.o:
+	@${CC} ${CFLAGS} -c $< -o $@
 
 $(NAME): $(OBJS)
 	@make all -C ./Libft
---
+++ cub3d.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnaidu <lnaidu@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 11:31:24 by lnaidu            #+#    #+#             */
+/*   Updated: 2023/05/26 11:35:12 by lnaidu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "./Libft/srclib/libft.h"
+# include "./Libft/gnl/get_next_line.h"
+# include <errno.h>
+# include <string.h>
+# include <math.h>
+# include <mlx.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <ctype.h>
+
+#endif
